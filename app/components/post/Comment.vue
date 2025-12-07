8d10e38cdf7903cbfd1327923422f3e740a132cb
enltlh <enltlh@gmail.com>
2025-12-07 00:14:10 +0000

    style: 简化twikoo样式

--- app/components/post/Comment.vue
+++ app/components/post/Comment.vue
@@ -25,10 +25,9 @@ onMounted(() => {
 
 <style lang="scss" scoped>
 .z-comment {
-	margin: 2rem auto;
-	padding: 0 1rem;
+	margin: 3rem 1rem;
 
-	h3 {
+	> h3 {
 		margin-top: 3rem;
 		font-size: 1.25rem;
 	}
@@ -55,72 +54,7 @@ onMounted(() => {
 	}
 
 	.tk-content {
-		font-size: 0.95rem;
-		line-height: 1.6;
-
-		.tk-owo-emotion {
-			width: auto;
-			height: 1.4em;
-			vertical-align: text-bottom;
-		}
-
-		a {
-			margin: 0 -0.1em;
-			padding: 0 0.1em;
-			background: linear-gradient(var(--c-primary-soft), var(--c-primary-soft)) no-repeat bottom/100% 0.1em;
-			color: var(--c-primary);
-			transition: all 0.2s;
-
-			&:hover {
-				border-radius: 0.3em;
-				background-size: 100% 100%;
-			}
-		}
-
-		p > code,
-		> code {
-			padding: 0.2em 0.4em;
-			border: 1px solid var(--c-border);
-			border-radius: 6px;
-			background: var(--c-bg-2);
-		}
-
-		.code-toolbar,
-		> span > pre {
-			position: relative;
-			overflow: auto;
-			padding: 0.4rem;
-			border: 2px solid var(--c-border);
-			border-radius: 8px;
-			background: var(--c-bg-2);
-
-			&::before { display: none; }
-
-			pre {
-				margin-top: 0.75rem;
-
-				code {
-					display: block;
-					padding-top: 0.75rem;
-				}
-			}
-		}
-
-		blockquote {
-			margin-bottom: 0.8rem;
-			padding: 0.8rem;
-			border-left: 4px solid var(--c-border);
-			border-radius: 8px;
-			background: var(--c-bg-2);
-			color: var(--c-text-2);
-			transition: all 0.2s;
-		}
-	}
-
-	.tk-comments-title { margin-bottom: 0; }
-
-	.tk-replies:not(.tk-replies-expand) {
-		mask: linear-gradient(#FFF 50%, transparent);
+		margin-top: 0.1rem;
 	}
 
 	.tk-expand {
@@ -130,104 +64,87 @@ onMounted(() => {
 		color: var(--c-text-1);
 		transition: background-color 0.1s;
 
-		&:hover { background-color: var(--c-bg-3); }
-	}
-
-	.tk-nick-link { color: var(--c-primary); }
-
-	.tk-comment .tk-main {
-		.tk-meta { margin-bottom: 0.3rem; }
-
-		.tk-extras {
-			margin-top: 0.5rem;
-			font-size: 0.85rem;
-			color: var(--c-text-2);
-		}
-
-		.tk-action .tk-action-link:first-child { display: none; }
-	}
-
-	.tk-preview,
-	.tk-cancel {
-		border: 1px solid var(--c-border);
-		border-radius: 8px;
-		background-color: var(--c-bg-2);
-		color: var(--c-text-1);
-
 		&:hover {
-			border-color: var(--c-primary);
 			background-color: var(--c-bg-3);
 		}
 	}
 
+	.tk-preview,
+	.tk-cancel,
 	.tk-send {
-		border: 1px solid var(--c-primary);
 		border-radius: 8px;
-		background-color: var(--c-primary);
-		color: white;
+	}
 
-		&:hover {
-			border-color: var(--c-primary-soft);
-			background-color: var(--c-primary-soft);
+	.tk-comments-title, .tk-nick > strong {
+		font-family: var(--font-creative);
+	}
+
+	.tk-owo-emotion {
+		width: auto;
+		height: 1.4em;
+		vertical-align: text-bottom;
+	}
+
+	.tk-extras, .tk-footer {
+		font-size: 0.7rem;
+		color: var(--c-text-3);
+	}
+
+	.tk-replies:not(.tk-replies-expand) {
+		mask-image: linear-gradient(#FFF 50%, transparent);
+	}
+}
+
+:deep(:where(.tk-preview-container,.tk-content)) {
+	pre {
+		overflow: auto;
+		border-radius: 0.5rem;
+		font-size: 0.8125rem;
+	}
+
+	p {
+		margin: 0.2em 0;
+	}
+
+	img {
+		border-radius: 0.5em;
+	}
+
+	menu, ol, ul {
+		margin: 0.5em 0;
+		padding-inline-start: 1.5em;
+		font-size: 0.9rem;
+		list-style: revert;
+
+		> li {
+			margin: 0.2em 0;
+
+			&::marker {
+				color: var(--c-primary);
+			}
 		}
 	}
-}
 
-@keyframes spin {
-	0% { transform: rotate(0); }
-	100% { transform: rotate(1turn); }
-}
-
-@keyframes fadeInTip {
-	from {
-		opacity: 0;
-		transform: translate(-50%, 10px);
+	blockquote {
+		margin: 0.5em 0;
+		padding: 0.2em 0.5em;
+		border-inline-start: 4px solid var(--c-border);
+		border-radius: 4px;
+		background-color: var(--c-bg-2);
+		font-size: 0.9rem;
 	}
 
-	to {
-		opacity: 1;
-		transform: translate(-50%);
-	}
-}
+	a {
+		margin: 0 -0.1em;
+		padding: 0 0.1em;
+		background: linear-gradient(var(--c-primary-soft), var(--c-primary-soft)) no-repeat bottom/100% 0.1em;
+		color: var(--c-primary);
+		transition: all 0.2s;
 
-@keyframes fadeInPanel {
-	from {
-		opacity: 0;
-		transform: translateY(-20px);
-	}
-
-	to {
-		opacity: 1;
-		transform: translateY(0);
-	}
-}
-
-:deep(#twikoo > :not(.tk-admin-container)) {
-	.tk-submit .tk-avatar {
-		display: block !important;
-		order: 0;
-		width: 48px;
-		height: 48px;
-		margin: 0 0.75rem 0 0 !important;
-	}
-
-	.tk-submit .tk-row.head {
-		display: flex;
-		align-items: flex-start;
-		order: 1;
-		width: 100%;
-	}
-
-	.tk-submit .tk-input {
-		order: 1;
-	}
-
-	.tk-submit .tk-meta-input {
-		order: 2;
-	}
-
-	.tk-submit .tk-row.actions {
-		order: 3;
+		&:hover {
+			border-radius: 0.3em;
+			background-size: 100% 100%;
+		}
 	}
 }
 </style>
